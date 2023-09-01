3ec24bcd3d89cd5d853cf43ffef8a71fe8c19fb4
LO-VE-0 <hsg151515@naver.com>
2023-09-01 13:29:25 +0900

    FEAT. js_study commit

---
+++ scripts/3-13(module).js
@@ -0,0 +1,18 @@
+// function log(message) {
+//     console.log(message);
+// }
+
+// function error(message) {
+//     console.error(message);
+// }
+
+
+export function log(message) {
+    console.log(message);
+}
+
+export function error(message) {
+    console.error(message);
+}
+
+export const PI = 3.14;
